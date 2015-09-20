d0431f0eec434e3d90747b99e4193ecbc2c996c9
mpradoa941 <pjovita8@gmail.com>
2015-09-20 20:30:33 +0000

    Template

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/userLogged'
+  root             'static_pages#home'
+  get 'userLogged'    => 'static_pages#userLogged'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
