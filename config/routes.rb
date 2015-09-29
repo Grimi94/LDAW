f1d8ef2fc34fc33fa73e419404f9f1b61a7c31d1
Antonio Grimaldo <grimi94@gmail.com>
2015-09-29 14:46:04 -0500

    Add: Working dummy store page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root             'static_pages#home'
   get 'userLogged'    => 'static_pages#userLogged'
+  get 'store'        => 'stores#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
