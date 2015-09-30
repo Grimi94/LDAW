7ca4907f2470b7d9f625e02237823294b1ddd988
Antonio Grimaldo <grimi94@gmail.com>
2015-09-30 13:25:30 -0500

    Add: controller and dynamic view for store

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,2 +1,11 @@
 class StoresController < ApplicationController
+
+  def show
+    @store = Store.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml {render :xml => @product}
+    end
+  end
 end
