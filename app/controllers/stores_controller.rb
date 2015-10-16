661e3ac6a991e9974fddb10cbeef3bde0ed1d104
mpradoa941 <pjovita8@gmail.com>
2015-10-16 22:16:43 +0000

    Average ratings and style changes

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,16 +3,18 @@ class StoresController < ApplicationController
   def show
     @store = Store.find(params[:id])
     @reviews = Review.where(store_id: @store.id).order("created_at DESC")
+    
+    if @reviews.blank?
+      @avg = 0
+    else
+      @avg = @reviews.average(:ratings).round
+    end
 
     respond_to do |format|
       format.html # show.html.erb
       format.xml {render :xml => @product}
     end
     
-    if @reviews.blank?
-      @avg_review = 0
-    else
-      @avg_review = @reviews.average(:ratings).round(2)
-    end
   end
+  
 end
