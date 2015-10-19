b0b20473e6f2110fd5c1415f48b99f8c7a9a9929
mpradoa941 <pjovita8@gmail.com>
2015-10-19 18:44:15 +0000

    Modify reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,6 +9,7 @@ class ReviewsController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -30,7 +31,7 @@ class ReviewsController < ApplicationController
   def update
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        format.html { redirect_to @store, notice: 'Review was successfully updated.' }
         format.json { render :show, status: :ok, location: @review }
       else
         format.html { render :edit }
