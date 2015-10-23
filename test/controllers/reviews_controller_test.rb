a648790ba54c0ac37f2cb448919f1a2052d825b3
mpradoa941 <pjovita8@gmail.com>
2015-10-23 22:30:28 +0000

    Reviews

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -5,12 +5,6 @@ class ReviewsControllerTest < ActionController::TestCase
     @review = reviews(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:reviews)
-  end
-
   test "should get new" do
     get :new
     assert_response :success
