503a0c7d9bab380d1eb47f458f62cf3bae51b48a
mpradoa941 <pjovita8@gmail.com>
2015-10-23 17:05:54 +0000

    Add admin boolean to users and admin can delte users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
+  before_action :verify_is_admin, only: [:index]
   
-  def edit
+  def index
+    @users = User.all
+  end
+  
+  def destroy
+    @users = User.find(params[:id])
+    @users.destroy
+
+    if @users.destroy
+        redirect_to allUsers_path, notice: "User deleted."
+    end
+  end
+  
+  private
+  
+  def verify_is_admin
+  (current_user.nil?) ? redirect_to(root_path) : (redirect_to(root_path) unless current_user.admin?)
   end
 
 end
