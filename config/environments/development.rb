456a5d6320d7257a367dc3bebff4501aae1036c6
Antonio Grimaldo <grimi94@gmail.com>
2015-10-23 18:17:28 -0500

    Allowed access to assets file on server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+    config.serve_static_assets = true
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
