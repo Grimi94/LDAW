3587718102f3898b735681635cd8d2fe224d38c5
Antonio Grimaldo <grimi94@gmail.com>
2015-10-23 19:26:33 -0500

    Fix: search query to accept lowercase

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,6 +3,6 @@ class Store < ActiveRecord::Base
     has_attached_file :image, styles: {large: "800x300>", medium: "320x150>", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
     def self.search(search)
-        where("name LIKE ?", "%#{search}%") 
+        where("lower(name) LIKE ?", "%#{search.downcase}%") 
     end
 end
