6886f43cb94925399738688789d30ae03d40eb6f
mpradoa941 <pjovita8@gmail.com>
2015-11-25 20:04:52 +0000

    Change maps and add review translation

--- db/schema.rb
+++ db/schema.rb
@@ -37,10 +37,10 @@ ActiveRecord::Schema.define(version: 20151125033252) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.integer  "user_id"
     t.float    "latitude"
     t.float    "longitude"
     t.string   "street"
+    t.integer  "user_id"
   end
 
   add_index "stores", ["user_id"], name: "index_stores_on_user_id", using: :btree
