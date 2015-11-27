2d747255960d7386d07221d4fa55ae162021a65e
Antonio Grimaldo <grimi94@gmail.com>
2015-11-26 18:43:43 -0600

    Add: new admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,43 +8,8 @@
 #
 #
 #
-Store.destroy_all
-Store.create!([
-  {
-    id: 1,
-    name: "Taqueria Arandas",
-    city: "Queretaro",
-    state: "Queretaro",
-    description: "Tacos como nunca los has probado, tenemos de pastor, bisteck, cordero, etc",
-    rating: 4.5,
-    approved: false
-  },
-  {
-    id:2,
-    name: "Maco Cafe",
-    city: "Queretaro",
-    state: "Queretaro",
-    description: "Ofrecemos cafe artesanal y ricos panes hechos a mano, ven a conocernos",
-    rating: 4.5,
-    approved: false
-  },
-  {
-    id:3,
-    name: "Optica San Pablo",
-    city: "Queretaro",
-    state: "Queretaro",
-    description: "Nos dedicamos a cuidar tus ojos",
-    rating: 4.5,
-    approved: false
-  },
-  {
-    id:4,
-    name: "Aguacatosas",
-    city: "Queretaro",
-    state: "Queretaro",
-    description: "Las mejores hamburguesas de Queretaro, si nunca has probado una aguacatosa, no has vivido..",
-    rating: 4.5,
-    approved: false
-  }
-  
+User.destroy_all
+User.create!([
+  {email: "testadmin@gmail.com", password: "testadminuser", password_confirmation: "testadminuser", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: true},
+  {email: "testcustomer@customer.co.uk", password: "testcustomeruser", password_confirmation: "testcustomeruser", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:03:44", last_sign_in_at: "2015-02-06 14:03:44", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: false}
 ])
