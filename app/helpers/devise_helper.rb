d26ea74320ed5b072aa4cf052f03ed9db367e5c2
Antonio Grimaldo <grimi94@gmail.com>
2015-11-26 18:51:42 -0600

    Fix: error messages when sign out

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,34 +1,28 @@
 module DeviseHelper
+ # A simple way to show error messages for the current devise resource. If you need
+ # to customize this method, you can either overwrite it in your application helpers or
+ # copy the views to your application.
+ #
+ # This method is intended to stay simple and it is unlikely that we are going to change
+ # it to add more behavior or options.
+ def devise_error_messages!
+   return "" if resource.errors.empty?
 
-  def devise_error_messages!
-    flash_alerts = []
-    error_key = 'errors.messages.not_saved'
-
-    if !flash.empty?
-      flash_alerts.push(flash[:error]) if flash[:error]
-      flash_alerts.push(flash[:alert]) if flash[:alert]
-      flash_alerts.push(flash[:notice]) if flash[:notice]
-      error_key = 'devise.failure.invalid'
-    end
-
-    return "" if resource.errors.empty? && flash_alerts.empty?
-    errors = resource.errors.empty? ? flash_alerts : resource.errors.full_messages
-
-    messages = errors.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t(error_key, :count    => errors.count,
-                                 :resource => resource.class.model_name.human.downcase)
+   messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+   sentence = I18n.t("errors.messages.not_saved",
+                     count: resource.errors.count,
+                     resource: resource.class.model_name.human.downcase)
 
     html = <<-HTML
-     <div class="alert alert-danger">
-        <a href="#" data-dismiss="alert" class="close">×</a>
-        <div id="error_explanation">
-          <h4>#{sentence}</h4>
-          <ul>#{messages}</ul>
-        </div>
-    </div>
-    HTML
+    <div class="alert alert-danger">
+       <a href="#" data-dismiss="alert" class="close">×</a>
+       <div id="error_explanation">
+         <h4>#{sentence}</h4>
+         <ul>#{messages}</ul>
+       </div>
+   </div>
+   HTML
 
-    html.html_safe
-  end
-
+   html.html_safe
+ end
 end
